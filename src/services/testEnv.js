cab2e434ba93ce26ec6ea5b7bbdeda04288bba60
HOME\Cheng.CuiCui <cheng.cuicui@eland.co.kr>
2018-03-01 14:22:02 +0800

    test-2

--- src/services/testEnv.js
+++ src/services/testEnv.js
@@ -1,5 +1,5 @@
 import { backendAddr } from '../utils/config';
 
 export function getEnv() {
-    return backendAddr.envs
+    return "API:" + process.env.API_ENV + " STAGE:" + process.env.STAGE_ENV + " NODE:" + process.env.NODE_ENV
 }
